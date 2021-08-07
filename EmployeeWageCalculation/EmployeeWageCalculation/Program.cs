74f62e0f25e62618f4be702d187046517a2e1a83
Bhaveshk <Bhavishkukreja16@gmail.com>
2021-08-07 12:46:06 +0530

    the UC1 commit

---
+++ EmployeeWageCalculation/EmployeeWageCalculation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeWageCalculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int is_full_day = 1;
+
+            Random random = new Random();
+            int day = random.Next(0, 2);
+
+            if (day == is_full_day)
+            {
+                Console.WriteLine("Employee is Present ");
+            }
+            else
+            {
+                Console.WriteLine("The Employee is Absent ");
+            }
+        }
+    }
+}
